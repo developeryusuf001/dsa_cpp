a5e498856ab0eee4f4721e894bb31e2953269f58
developeryusuf001 <mohammadyusufcse001@gmail.com>
2026-04-18 19:04:33 +0530

    user input problems

---
+++ practise/age.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    
+    cout << "Enter your name " << endl;
+    cin >> name;
+
+    cout << "Enter your age " << endl;
+    cin >> age;
+
+    cout << "Hello " << name << ","<< "Your age is  " << age << endl;
+
+}
